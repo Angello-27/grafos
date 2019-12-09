2ba18846682995f3a94d64c7ff3bb48f0de7a6c9
Miguel Angel <m.escobar_27@outlook.com>
2019-12-09 13:52:45 -0400

    probando la estructura del grafo

--- __init__.py
+++ __init__.py
@@ -6,12 +6,12 @@ for i in range(6):
 
 g.enlazar(0, 1, 0.5)
 g.enlazar(1, 2, 0.4)
-g.enlazar(2, 1, 0.2)
+g.enlazar(2, 1, 0.9)
 g.enlazar(0, 5, 0.9)
 g.enlazar(1, 5, 0.3)
 g.enlazar(0, 4, 0.8)
 g.enlazar(5, 0, 0.1)
-g.enlazar(3, 3, 0.6)
+g.enlazar(4, 3, 0.6)
 g.enlazar(0, 4, 0.7)
 
 for v in g:
